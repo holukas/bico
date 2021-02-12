abe4e8404162e94ab649a51544166a6e3b371067
holukas <lukas.hoertnagl@usys.ethz.ch>
2021-02-12 15:57:41 +0100

    Fixed version date

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     name='bico',
     packages=setuptools.find_packages(),
     # packages=['dyco'],
-    version='0.3.1',
+    version='0.3.2',
     license='GNU General Public License v3 (GPLv3)',
     description='A Python package to convert binary files to ASCII',
     long_description=long_description,
