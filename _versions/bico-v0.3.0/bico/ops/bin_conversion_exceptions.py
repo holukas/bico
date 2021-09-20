ec311f4633df4715130703d10104c845dc35b630
holukas <lukas.hoertnagl@usys.ethz.ch>
2021-09-20 10:09:10 +0200

    More cleanup 2

--- _versions/bico-v0.3.0/bico/ops/bin_conversion_exceptions.py
+++
@@ -1,7 +0,0 @@
-def dblock_r2a_t_sonic(var_val):
-    """Special conversion for T_SONIC in R2-A"""
-    var_val *= 0.02
-    var_val *= var_val
-    var_val /= 403
-    var_val -= 273.15  # To °C
-    return var_val
--- _versions/bico-v0.3.0/bico/settings/_version.py
+++
@@ -1,6 +0,0 @@
-__version__ = "0.3.0"
-__date__ = "15 Jan 2021"
-__link_source_code__ = "https://gitlab.ethz.ch/holukas/bico"
-__link_releases__ = "https://gitlab.ethz.ch/holukas/bico/-/releases"
-__link_wiki__ = "https://gitlab.ethz.ch/holukas/bico/-/wikis/home"
-__license__ = "https://gitlab.ethz.ch/holukas/bico/-/blob/master/LICENSE"
