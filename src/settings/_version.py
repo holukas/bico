3bd0cc4f0fbaf6d4044978be1418caceb6e6e55f
Lukas Hörtnagl <lukas.hoertnagl@usys.ethz.ch>
2024-03-21 15:46:12 +0100

    Update _version.py

--- src/settings/_version.py
+++ src/settings/_version.py
@@ -1,8 +1,8 @@
 # from single_source import get_version
 # __version__ = get_version(__name__, Path(__file__).parent.parent.parent)  # Single source of truth for version
 
-__version__ = "1.6.0"
-__date__ = "14 Nov 2023"
+__version__ = "1.6.2"
+__date__ = "21 Mar 2024"
 __link_changelog__ = "https://github.com/holukas/bico/blob/master/CHANGELOG.md"
 __link_source_code__ = "https://github.com/holukas/bico"
 __link_releases__ = "https://github.com/holukas/bico/releases"
